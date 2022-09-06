916b97d866ec13eba858db9cf0236674ff6521ab
AstrikTorres <astriktorres@gmail.com>
2022-09-06 01:24:34 -0500

    Agrega propiedades a las columnas de la entidad User

--- src/main/java/com/gestopago/springboot/model/User.java
+++ src/main/java/com/gestopago/springboot/model/User.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 @Entity
 @Table(name = "users")
@@ -11,10 +13,19 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false, unique = true, updatable = false)
     private long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
     private String password;
-    private String role;
+
+    // Default value is 'USER'
+    @Column(nullable = false)
+    private String role = "USER";
 
 }
